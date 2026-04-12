9d78b8673afc048e4000832e8e345b6d9529676b
Arnav183 <Arnav183@users.noreply.github.com>
2026-04-12 20:55:16 +0000

    deploy: b9b4c8badc5a7c26618d149ab7fe728e7d1cc787

---
+++ assets/EmployerOnboarding-C3hcCREE.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-Ivs8YJJ7.js";const r="https://docs.google.com/forms/d/1uz55KEIkH3XwnVJxvdQgMMByidmsQRB9dRkVWLJb8p0/viewform";function e(){return o.useEffect(()=>{window.location.replace(r)},[]),null}export{e as default};
---
+++ assets/WorkspaceLogin-qjE2HrFN.js
@@ -0,0 +1,1 @@
+import{u as t,r as a}from"./index-Ivs8YJJ7.js";function o(){const e=t();return a.useEffect(()=>{e("/login",{replace:!0})},[e]),null}export{o as default};
